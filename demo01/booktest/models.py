2a04d7744630bddd42967ed6a160b8cafe37ea8c
dh <2964727542@qq.con>
2019-04-16 17:26:05 +0800

    执行迁移

--- demo01/booktest/models.py
+++ demo01/booktest/models.py
@@ -7,17 +7,17 @@ from django.db import models
 
 
 class Author(models.Model):
-    name = models.CharField(10)
-    gender = models.CharField(10)
+    name = models.CharField(max_length=10)
+    gender = models.CharField(max_length=10)
 
     def __str__(self):
         return self.name
 
 
 class Book(models.Model):
-    bookname = models.CharField(10)
+    bookname = models.CharField(max_length=10)
     author = models.ForeignKey('Author', on_delete=models.CASCADE)
-    brief_info = models.CharField(200)
+    brief_info = models.CharField(max_length=200)
     writing_time = models.DateTimeField()
 
     def __str__(self):
