10281268b99ea8f6140a70363705f7ec76967997
dh <2964727542@qq.con>
2019-04-16 16:58:35 +0800

    为迁移文件作准备

--- demo01/booktest/models.py
+++ demo01/booktest/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+"""
+创建新的类
+"""
+
+
+class Author(models.Model):
+    name = models.CharField(10)
+    gender = models.CharField(10)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    bookname = models.CharField(10)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    brief_info = models.CharField(200)
+    writing_time = models.DateTimeField()
+
+    def __str__(self):
+        return self.bookname
+
+
+
+
