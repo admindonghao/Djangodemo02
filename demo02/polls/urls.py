a68e4b2c91ba5fa33b442435c75c92dba04c58ca
dh <2964727542@qq.con>
2019-04-18 15:44:37 +0800

    功能实现1

--- demo02/polls/urls.py
+++ demo02/polls/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from .views import *
 
+app_name = 'polls'
 
 urlpatterns = [
     url(r'^$', index, name='index'),
+    url(r'^list/$', list, name='list'),
+    url(r'^detail/(\d+)/$', detail, name='detail'),
 ]
--- demo02/polls/views.py
+++ demo02/polls/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import IssueList, ChioceList
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse('首页')
+    return render(request, 'polls/index.html', {})
 
+
+def list(request):
+    pl = IssueList.objects.all()
+    return render(request, 'polls/list.html', {'polls':pl})
+
+
+def detail(request, id):
+    poll = IssueList.objects.get(pk=id)
+    return render(request, 'polls/detail.html', {'poll':poll})
