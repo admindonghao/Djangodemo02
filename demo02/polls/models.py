a5ecdc0a1f10358cac07dd25344f8c11bc77b895
dh <2964727542@qq.con>
2019-04-18 14:30:02 +0800

    添加类，使用迁移创建数据库

--- demo02/polls/models.py
+++ demo02/polls/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class IssueList(models.Model):
+    issue = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.issue
+
+
+class ChioceList(models.Model):
+    options = models.CharField(max_length=20)
+    number = models.IntegerField(default=0)
+    is_issue = models.ForeignKey('IssueList', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.options
+
